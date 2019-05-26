9b8334e6d171f74d11022db9ec92210236891866
bcabanayan <bcabanayan@gmail.com>
2019-05-26 16:19:15 -0700

    Removed test functions from iterative_sorting.py

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,13 +7,11 @@ def selection_sort( arr ):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         for j in range(cur_index + 1, len(arr)):
-            if arr[smallest_index] > arr[j]:
+            if arr[j] < arr[smallest_index]:
                 smallest_index = j
         # TO-DO: swap
         arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
     return arr
-
-print(selection_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
@@ -33,6 +31,4 @@ def bubble_sort( arr ):
 def count_sort( arr, maximum=-1 ):
 
     return arr
-
-print(bubble_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 
