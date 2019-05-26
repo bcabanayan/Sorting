8e7c25f7b294b17064a4a9c9bcd35bdee702d2b3
bcabanayan <bcabanayan@gmail.com>
2019-05-26 11:00:33 -0700

    Wrote selection sort code to find smallest element in unsorted array

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,18 +6,14 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        
-    
-             
-
-
-
+        for j in range(cur_index + 1, len(arr) - 1):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
         # TO-DO: swap
-
-
-
-
+        print (arr[smallest_index])
     return arr
+
+selection_sort([3,5,2,5,6,1,4,3,6])
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
